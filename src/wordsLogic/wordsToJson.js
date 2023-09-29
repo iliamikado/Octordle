940a88c4b879e6073c57d86f351c16f3105ed90b
iliamikado <iliamikado@gmail.com>
2023-09-29 10:19:31 +0300

    new word

--- src/wordsLogic/wordsToJson.js
+++ src/wordsLogic/wordsToJson.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const words = fs.readFileSync('src/wordsLogic/words.txt').toString().split('\n').map(x => (x.split(',')));
+const words = fs.readFileSync('src/wordsLogic/words.txt').toString().split('\n').map(x => (x.trim().split(',')));
 words.sort()
+console.log(words);
 const easyWords = words.filter(x => (x[1] === '1')).map(x => (x[0]));
 const hardWords = words.filter(x => (x[1] === '2')).map(x => (x[0]));
 fs.writeFileSync('src/wordsLogic/words.json', JSON.stringify({easyWords, hardWords}));
