d73e519c69a2a14b662ed639f45a5f0e3b3fa52b
iliamikado <iliamikado@gmail.com>
2023-10-23 23:18:28 +0300

    exp

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,6 +1,8 @@
 const url = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:5000';
+console.log(process.env.NEXT_PUBLIC_SERVER_URL);
 
 export function postGameResult(result: {day: number, words: string, tries: string, score: number}) {
+    console.log(process.env.NEXT_PUBLIC_SERVER_URL);
     fetch(`${url}/post_game`, {
         method: 'POST',
         headers: {
