d7e101fb0576018db109c13ade1564bf4f26d7f9
iliamikado <iliamikado@gmail.com>
2023-11-09 15:43:28 +0300

    deploy client on host

--- server/src/server.js
+++ server/src/server.js
@@ -11,28 +11,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/ping', (req, res) => {
+app.get('/api/ping', (req, res) => {
     res.send('I am alive');
 })
 
-app.post('/post_game', async (req, res) => {
+app.post('/api/post_game', async (req, res) => {
     const gameInfo = req.body;
     await statistics.addTodaysGame(gameInfo);
     res.json(await statistics.getStatForGame(gameInfo));
 });
 
-app.post('/post_start', async (req, res) => {
+app.post('/api/post_start', async (req, res) => {
     console.log(req.body);
     const gameStart = req.body;
     await statistics.addStartedGame(gameStart);
     res.json({status: 200});
 })
 
-app.get('/get_game_stat', async (req, res) => {
+app.get('/api/get_game_stat', async (req, res) => {
     res.json(await statistics.getStatForGame(JSON.parse(req.query.game)));
 })
 
-app.get('/get_full_stat', async (req, res) => {
+app.get('/api/get_full_stat', async (req, res) => {
     console.log(req.query);
     res.json(await statistics.getFullStat(req.query.uuid));
 })
--- src/service/service.ts
+++ src/service/service.ts
@@ -1,4 +1,4 @@
-const url = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:5000';
+const url = (process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:5000') + '/api';
 
 export function postGameResult(result: {day: number, words: string, tries: string, score: number, uuid: string}) {
     return fetch(`${url}/post_game`, {
