fadb8a65a9785e985e22269274c68a95196e05ba
iliamikado <iliamikado@gmail.com>
2023-11-18 21:02:54 +0300

    fix losers

--- server/src/statistic.js
+++ server/src/statistic.js
@@ -15,16 +15,15 @@ class Statistic {
     async getStatForGame(game) {
         let losers = 0;
         const day = Math.floor(Date.now() / 1000 / 60 / 60 / 24) - START_DAY;
-        const todayGames = await GameInfo.findAll({where: {day}});
-        const uuidsSet = new Set(todayGames.map(({uuid}) => (uuid)));
-        const startedGames = (await StartedGame.findAll({where: {day}})).filter(({uuid}) => (!uuidsSet.has(uuid))).filter(({score}) => (score < 124));
+        const todayGames = await GameInfo.findAll({where: {day}}).filter(({score}) => (score < 124));
+        const startedGames = (await StartedGame.findAll({where: {day}}));
 
         todayGames.forEach(({score}) => {
             if (game.score >= score) {
                 losers += 1;
             }
         });
-        losers += startedGames.length;
+        losers += startedGames.length - todayGames.length;
         const resp = {};
         const n = todayGames.length + startedGames.length;
         if (n <= 1) {
