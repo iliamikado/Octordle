e3ed56f4a8c295f08b2f139f1690cfcb333d6aa1
iliamikado <iliamikado@gmail.com>
2023-11-19 23:43:49 +0300

    fix stats

--- server/src/statistic.js
+++ server/src/statistic.js
@@ -13,23 +13,17 @@ class Statistic {
     }
 
     async getStatForGame(game) {
-        let losers = 0;
         const day = Math.floor(Date.now() / 1000 / 60 / 60 / 24) - START_DAY;
-        const todayGames = (await GameInfo.findAll({where: {day}})).filter(({score}) => (score < 124));
+        const games = (await GameInfo.findAll({where: {day}})).filter(({score}) => (score < 124));
         const startedGames = (await StartedGame.findAll({where: {day}}));
-
-        todayGames.forEach(({score}) => {
-            if (game.score >= score) {
-                losers += 1;
+        let losers = startedGames.length;
+        games.forEach(({score}) => {
+            if (score > game.score) {
+                losers--;
             }
-        });
-        losers += startedGames.length - todayGames.length;
-        const resp = {};
-        const n = todayGames.length + startedGames.length;
-        if (n <= 1) {
-            resp.betterThan = 100;
-        } else {
-            resp.betterThan = Math.floor(losers * 100 / n);
+        })
+        const resp = {
+            betterThan: Math.floor(losers * 100 / startedGames.length)
         }
         return resp;
     }
@@ -67,7 +61,6 @@ class Statistic {
             resp.min = Math.min(resp.min, score);
         });
         games.sort((g1, g2) => (g1.score - g2.score));
-        console.log(games);
         resp.median = games[Math.floor(games.length / 2)]?.score;
 
         const userGame = games.find(({uuid: id}) => (id === uuid));
@@ -79,7 +72,6 @@ class Statistic {
         resp.place = [1, 0];
         let losers = startedGames.length;
         resp.timePlace = 1;
-        console.log(userGame.createdAt);
         games.forEach(({score, createdAt}) => {
             if (score > userGame.score) {
                 resp.place[0]++;
