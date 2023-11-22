f44d67b007e7550380ced62d24c393dd910f1d99
iliamikado <iliamikado@gmail.com>
2023-11-22 13:45:33 +0300

    chart js

--- src/appPages/StatsPage/StatsPage.tsx
+++ src/appPages/StatsPage/StatsPage.tsx
@@ -4,16 +4,17 @@ import cn from 'classnames';
 import styles from './StatsPage.module.scss';
 import CrossIcon from './assets/cross.svg';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { getFullStat } from '@/service/service';
 import { useAppSelector } from '@/store/store';
 import { selectUuid } from '@/store/selectors';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 export const StatsPage = () => {
     const [stats, setStats] = useState<any>({loading: true, error: false});
     const uuid = useAppSelector(selectUuid);
-    const [graphData, setGraphData] = useState<{uv: number, name?: number}[]>([]);
+    const chart = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
         if (!uuid) {
@@ -21,18 +22,49 @@ export const StatsPage = () => {
         }
         getFullStat(uuid).then((stats) => {
             setStats(stats);
-            const scores = stats.personal.scores;
-            // const data = scores.map(([day, score]: [number, number]) => ({uv: score, name: day}));
-            const data = [];
-            const startDay = scores[0][0];
-            for (let i = 0; i < scores.at(-1)[0] - scores[0][0]; ++i) {
-                data.push({uv: 0});
-            }
-            scores.forEach(([day, score]: [number, number]) => {
-                data[day - startDay] = {uv: score, name: day}
-            });
-            setGraphData(data);
+            setTimeout(() => {
+                if (!chart.current) {
+                    return;
+                }
 
+                const ctx = chart.current.getContext('2d');
+                if (!ctx) {
+                    return;
+                }
+
+                const scores = [];
+                for (let i = stats.personal.scores[0][0]; i < stats.personal.scores.at(-1)[0]; ++i) {
+                    scores.push(0);
+                }
+                stats.personal.scores.forEach(([day, score]: [number, number]) => {
+                    scores[day - stats.personal.scores[0][0]] = score;
+                });
+                new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: scores.map(() => ('')),
+                        datasets: [
+                            {
+                                data: scores,
+                                fill: false,
+                                borderColor: 'rgb(75, 192, 192)',
+                                tension: 0.1,
+                                pointStyle: false
+                            }
+                        ]
+                    },
+                    options: {
+                        plugins: {
+                            legend: {
+                                display: false,
+                            },
+                            tooltip: {
+                                enabled: false
+                            }
+                        }
+                    }
+                })
+            }, 500)
         }).catch(e => {
             setStats({loading: false, error: true});
         });
@@ -57,12 +89,9 @@ export const StatsPage = () => {
                 <h3 style={{margin: 0}}>Личная Статистика</h3>
                 <p>Игр сыграно: {stats.personal.played}</p>
                 <p>Средний балл: {stats.personal.average}</p>
-                <LineChart width={550} height={400} data={graphData} style={{width: '100%'}}>
-                    <Line type="monotone" dataKey="uv" stroke="#8884d8" dot={false}/>
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                </LineChart>
+                <p>Баллы за все время:</p>
             </div>
+            <canvas ref={chart} style={{width: '100%', height: '300px'}}></canvas>
         </div>}
     </div>
 }
