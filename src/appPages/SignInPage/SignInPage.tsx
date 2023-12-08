70b9362f0bc9f946ee28741095bbd587053d40bf
iliamikado <iliamikado@gmail.com>
2023-12-08 17:37:10 +0300

    make logout button smaller

--- src/appPages/SignInPage/SignInPage.tsx
+++ src/appPages/SignInPage/SignInPage.tsx
@@ -41,13 +41,13 @@ export const SignInPage = () => {
             <CrossIcon/>
         </button>
 
-        {userInfo ? <div className={styles.block}>
+        {userInfo ? <div className={styles.logout}>
             <p>Вы вошли как {userInfo.name}</p>
             <button onClick={() => {
                 dispath(setUserInfo(null));
                 localStorage.removeItem('name');
                 localStorage.removeItem('email');
-            }} className={styles.googleAuth}>Logout</button>
+            }}>Logout</button>
         </div> : <div className={styles.block}>
             <p>Мы не знаем, кто вы. Войдите с помощью аккаунта Google, чтобы переносить свой прогресс на разные устройства</p>
             <button onClick={() => {googleAuth()}} className={styles.googleAuth}>Sign in with google</button>
